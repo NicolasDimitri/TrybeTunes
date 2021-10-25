39b312854c03dcff5fcce0def1fdda157b7f5800
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2021-10-24 21:01:14 -0300

    recomeçando o projeto

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './pages/Content';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      search: false,
-    };
-    this.searchPage = this.searchPage.bind(this);
-  }
-
-  searchPage() {
-    this.setState({
-      search: true,
-    });
-  }
-
   render() {
-    const { search } = this.state;
-
     return (
-      <BrowserRouter>
-        <p>TrybeTunes</p>
-        <Content search={ search } searchPage={ this.searchPage } />
-      </BrowserRouter>
+      <p>trybetunes</p>
     );
   }
 }
