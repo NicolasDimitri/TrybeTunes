40315406bb8e918aa4e7f89b3b2907658248bc3a
Nicolas Dimitri <nicolasdodimitri@gmail.com>
2021-10-27 16:35:00 -0300

    requisito 7 concluido

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      musicList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMusicList();
+  }
+
+  async getMusicList() {
     const {
       match: {
         params: {
@@ -11,11 +23,34 @@ export default class Album extends Component {
         },
       },
     } = this.props;
-    console.log(id);
+    const musicList = await getMusics(id);
+    this.setState({
+      musicList,
+    });
+  }
+
+  render() {
+    const {
+      musicList,
+    } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        {musicList.length > 0
+&& (
+  <>
+    <h1 data-testid="artist-name">{musicList[0].artistName}</h1>
+    <h3 data-testid="album-name">{musicList[0].collectionName}</h3>
+    {musicList.slice(1).map((album) => (
+      <div key={ album.artistId }>
+        <p>{album.trackName}</p>
+        <audio data-testid="audio-component" src={ album.previewUrl } controls>
+          <track kind="captions" />
+          <code>audio</code>
+        </audio>
+      </div>
+    ))}
+  </>)}
       </div>
     );
   }
